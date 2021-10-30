4e725650da8b9c78c84c3fafe900d2df3c3dd540
taher zidane <taherzidane@tahers-MacBook-Pro.local>
2021-10-29 18:10:00 -0700

    first commit

---
+++ lib/layout/home_layout.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: override_on_non_overriding_member
+
+import 'package:flutter/material.dart';
+
+class NewsLayout extends StatelessWidget {
+  const NewsLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
